fcfdf576052f1530a31fc73abd08448ddae6ffc8
Tom Cohen <Tomco@noal.co.il>
2020-04-28 17:55:34 +0100

    final assignment!

--- C10219 - Concentration Game/GameViewController.swift
+++ C10219 - Concentration Game/GameViewController.swift
@@ -16,21 +16,26 @@ class GameViewController: UIViewController,UICollectionViewDelegate,UICollection
     var model = CardModel()
     
     @IBOutlet weak var main_LBL_timer: UILabel!
+    var firstCardFlipped:IndexPath?
+    
     
     var timer:Timer?
-    var milis:Float = 10*1000
-    var firstCardFlipped:IndexPath?
+    var milis:Float = 35*1000
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //get randomized new deck of cards
-        cards = model.getDeck()
+        //cards = model.getDeck()
+        resetGame()
+//        main_CV_cards.delegate = self
+//        main_CV_cards.dataSource = self
         
-        main_CV_cards.delegate = self
-        main_CV_cards.dataSource = self
-        
-        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timerElapsed), userInfo: nil, repeats: true)
+        //timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timerElapsed), userInfo: nil, repeats: true)
+        //RunLoop.main.add(timer!, forMode: .common)
         
     }
     //MARK: UICollectionView Related Protocols:
@@ -49,6 +54,8 @@ class GameViewController: UIViewController,UICollectionViewDelegate,UICollection
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if milis <= 0{
+            return}
         let cell = main_CV_cards.cellForItem(at: indexPath)  as! CardCollectionViewCell
         let card = cards[indexPath.row]
         if card.isShown == false && card.isMatched == false {
@@ -79,6 +86,8 @@ class GameViewController: UIViewController,UICollectionViewDelegate,UICollection
             
             cell1?.remove()
             cell2?.remove()
+            
+            isGameEnded()
         } else{
             card1.isShown=false
             card2.isShown=false
@@ -94,5 +103,71 @@ class GameViewController: UIViewController,UICollectionViewDelegate,UICollection
         firstCardFlipped = nil
     }
     
-   
+    func isGameEnded(){
+        var isWon = true
+        
+        for card in cards {
+            if card.isMatched == false
+            {
+                isWon=false
+                break
+            }
+        }
+        
+        var title = ""
+        var message = ""
+        
+        if isWon{
+            timer?.invalidate()
+            title = "Congratulations!"
+            message = "You've Won!"
+            
+        }else{
+            if milis > 0{
+                return
+            }
+            title = "Game Over!"
+            message = "You've Lost!"
+        }
+        
+        showAlert(title, message)
+        
+    }
+    
+    func showAlert(_ title:String, _ message:String)
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        //let alertAction = UIAlertAction(title: "Ok", style: .default, handler: )
+        let alertAction = UIAlertAction(title: "Start Again!", style: .default, handler: {(alert: UIAlertAction!) in self.resetGame()})
+        alert.addAction(alertAction)
+        present(alert,animated: true, completion: nil)
+    }
+    
+    func resetGame()
+    {
+        cards = model.getDeck()
+        milis = 35*1000
+        main_CV_cards.delegate = self
+        main_CV_cards.dataSource = self
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(timerElapsed), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer!, forMode: .common)
+        firstCardFlipped = nil
+        main_CV_cards.reloadData()
+    }
+    
+    //MARK: Timer:
+    @objc func timerElapsed(){
+        milis -= 1
+        
+        let seconds = String(format: "%.2f", milis/1000)
+        main_LBL_timer.text = "\(seconds)"
+        
+        if milis <= 0{
+            timer?.invalidate()
+            
+            isGameEnded()
+        }
+    }
+    
+    
 }
